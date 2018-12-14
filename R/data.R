a51f7a7793c221ea41d10f24fdc90bc15c32793f
Charlotte Wickham <cwickham@gmail.com>
2018-12-14 11:36:01 -0800

    add data and basic docs

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Oregon House Representatives
+#'
+#' A dataset containing the chronological list of Oregon House representatives 
+#' since 1849 to present.
+#' 
+#' @format A tibble with 7381 rows and 13 variables where each row represents 
+#' a legislator in a particular legislative session:
+#' \describe{
+#'   \item{session_name}{Legislative session}
+#'   \item{Legislator}{Name of representative}
+#'   \item{Distric}{The legislative district they represented in this session}
+#'   ...
+#' }
+#' @source \url{https://www.oregonlegislature.gov/chief-clerk/Pages/representatives.aspx}
+"representatives"
